8f02dc6a36375bf6ec8af1734aa0ae8640f9dc38
Lasana Murray <lmurray@quenk.com>
2019-03-10 10:53:26 -0400

    chore: rebuild

--- lib/app/index.ts
+++ lib/app/index.ts
@@ -5,6 +5,7 @@
  */
 
 /** imports */
+import { merge } from '@quenk/noni/lib/data/record';
 import { System } from '@quenk/potoo/lib/actor/system';
 import {
     AbstractSystem,
@@ -14,6 +15,7 @@ import {
 import { Runtime } from '@quenk/potoo/lib/actor/system/vm/runtime';
 import { State } from '@quenk/potoo/lib/actor/system/state';
 import { Actor } from '@quenk/potoo/lib/actor';
+import { Flags } from '@quenk/potoo/lib/actor/context';
 import { Template } from './actor/template';
 import { Context } from './state/context';
 
@@ -31,6 +33,14 @@ export interface App extends System<Context> { }
 export abstract class JApp extends AbstractSystem<Context> implements App {
 
     state: State<Context> = newState(this);
+
+    flags: Partial<Flags> = {immutable:true, buffered:false};
+
+    init(c: Context): Context {
+
+        return merge(c, { flags: merge(c.flags, this.flags) });
+
+    }
 
     allocate(
         a: Actor<Context>,

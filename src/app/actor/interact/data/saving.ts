cc7340b37bd856024710ffa91283fa9074794a60
Lasana Murray <lmurray@quenk.com>
2019-03-14 09:14:45 -0400

    refactor(app/actor/interact/data/saving): extend Actor interface

--- src/app/actor/interact/data/saving.ts
+++ src/app/actor/interact/data/saving.ts
@@ -1,15 +1,16 @@
-import {Case} from '@quenk/potoo/lib/actor/resident/case';
+import { Case } from '@quenk/potoo/lib/actor/resident/case';
+import { Actor } from '../../';
 
 /**
  * BeforeSaving means the actor has a hook to invoke before transitioning
  * to saving.
  */
-export interface BeforeSaving<T> {
+export interface BeforeSaving<T> extends Actor {
 
-  /**
-   * beforeSaving
-   */
-  beforeSaving(t:T): BeforeSaving <T>
+    /**
+     * beforeSaving
+     */
+    beforeSaving(t: T): BeforeSaving<T>
 
 }
 
@@ -17,11 +18,11 @@ export interface BeforeSaving<T> {
  * Saving indicates an actor has a behaviour it can transition to while
  * saving data either remotely or locally.
  */
-export interface Saving<T,M> {
+export interface Saving<T, M> extends Actor {
 
-  /**
-   * saving behaviour
-   */
-  saving(t:T) : Case<M>
+    /**
+     * saving behaviour
+     */
+    saving(t: T): Case<M>
 
 }

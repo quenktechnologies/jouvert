27ace42c0b3872fec65b4b66a80c7eebe0a5efa1
Lasana Murray <lmurray@quenk.com>
2019-03-14 15:00:33 -0400

    refactor(app/actor/interact/data/form): split up form interfaces

--- src/app/actor/interact/data/form/index.ts
+++ src/app/actor/interact/data/form/index.ts
@@ -1,9 +1,29 @@
+/**
+ * Form interface is for actors that provide form
+ * functionality.
+ *
+ * Forms here are not concerned with the details of design and UX,
+ * just the workflow for capturing input.
+ *
+ * The form apis are designed around a client server model where another
+ * interact (the client) yields control to the form and awaits some message 
+ * indicating the form has been saved or aborted.
+ *
+ * Behaviour matrix:
+ *             suspended  resume  saving
+ * suspended                     
+ * resume      <Abort>    <Input> <Save>   
+ */
+
+/** imports */
 import { Constructor } from '@quenk/noni/lib/data/type/constructor';
 import { Address } from '@quenk/potoo/lib/actor/address';
 import { Case } from '@quenk/potoo/lib/actor/resident/case';
+import { Suspended } from '../../suspended';
 import { Resumed } from '../../resumed';
+import { Actor } from '../../../';
 import { Resume } from '../../';
-import { OnInput } from './on-input';
+import { OnInput } from './oninput';
 
 /**
  * ResumedMessages type.
@@ -12,6 +32,67 @@ export type ResumedMessages<E, MResumed>
     = E
     | MResumed
     ;
+
+/**
+ * OnInput interface has a handler for capturing user input.
+ */
+export interface OnInput<E> extends Actor {
+
+    /**
+     * onInput handler.
+     */
+    onInput(e: E): OnInput<E>
+
+}
+
+/**
+ * BeforeSaving means the actor has a hook to invoke before transitioning
+ * to saving.
+ */
+export interface BeforeSaving<T> extends Actor {
+
+    /**
+     * beforeSaving
+     */
+    beforeSaving(t: T): BeforeSaving<T>
+
+}
+
+/**
+ * Saving indicates an actor has a behaviour it can transition to while
+ * saving data either remotely or locally.
+ */
+export interface Saving<T, M> {
+
+    /**
+     * saving behaviour
+     */
+    saving(t: T): Case<M>[]
+
+}
+
+/**
+ * InputListener
+ */
+export interface InputListener<E, T, MResumed>
+    extends OnInput<E>, Resumed<T, MResumed> { }
+
+/**
+ * SaveListener
+ */
+export interface SaveListener<T, M> extends BeforeSaving<T>, Saving<T, M> { }
+
+/**
+ * AbortListener
+ */
+export interface AbortListener<A, M> extends Suspended<M> {
+
+    /**
+     * afterAbort hook.
+     */
+    afterAbort(a: A): AbortListener<A, M>
+
+}
 
 /**
  * Request extends the regular Resume 
@@ -32,29 +113,14 @@ export interface Request extends Resume {
 }
 
 /**
- * Inputtable 
+ * OnInputForm 
  */
-export interface Inputtable<E, T, MResumed>
+export interface OnInputForm<E, T, MResumed>
     extends OnInput<E>, Resumed<T, MResumed> { }
 
-/**
- * Form interface is for actors that provide form
- * functionality.
- *
- * Forms here are not concerned with the details of design and UX,
- * just the workflow for capturing input.
- *
- * The Form apis are designed around a client
- * server model where another Interact (the client) yields control
- * for input and awaits some message from the form indicating completion.
- *
- * Behaviour matrix:
- *             suspended  resume
- * suspended               <R>
- * resume                  <E>  
- */
+
 export interface Form<E, T, MResumed>
-    extends Inputtable<E, T, ResumedMessages<E, MResumed>> { }
+    extends OnInputForm<E, T, ResumedMessages<E, MResumed>> { }
 
 /**
  * InputCase applies the onInput hook and continues resuming.
@@ -64,12 +130,55 @@ export class InputCase<E, T, MResumed> extends Case<E> {
     constructor(
         public pattern: Constructor<E>,
         public token: T,
-        public form: Form<E, T, MResumed>) {
+        public form: InputListener<E, T, MResumed>) {
 
-        super(pattern, (e: E) =>
-            form
-                .onInput(e)
-                .select(form.resumed(token)));
+        super(pattern, (e: E) => {
+
+            form.onInput(e);
+            form.select(form.resumed(token));
+
+        });
+
+    }
+
+}
+
+/**
+ * SaveCase applies the beforeSaving hook and transitions to saving.
+ */
+export class SaveCase<S, MSaving> extends Case<S> {
+
+    constructor(
+        public pattern: Constructor<S>,
+        public listener: SaveListener<S, MSaving>) {
+
+        super(pattern, (s: S) => {
+
+            listener.beforeSaving(s);
+            listener.select(listener.saving(s));
+
+        });
+
+    }
+
+}
+
+/**
+ * AbortCase applies the afterAbort hook then transitions to 
+ * suspended.
+ */
+export class AbortCase<A, MSuspended> extends Case<A> {
+
+    constructor(
+        public pattern: Constructor<A>,
+        public listener: AbortListener<A, MSuspended>) {
+
+        super(pattern, (a: A) => {
+
+            listener.afterAbort(a);
+            listener.select(listener.suspended());
+
+        });
 
     }
 

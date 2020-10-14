4cede7716497eff5bedf4715518be21db4e7b519
Lasana Murray <lmurray@quenk.com>
2020-10-13 21:17:25 -0400

    refactor: updates the remote apis based on past experience

---
+++ src/app/remote/index.ts
@@ -0,0 +1,175 @@
+import { Err } from '@quenk/noni/lib/control/error';
+import {
+    parallel,
+    raise,
+    sequential
+} from '@quenk/noni/lib/control/monad/future';
+import { Case } from '@quenk/potoo/lib/actor/resident/case';
+import { Address } from '@quenk/potoo/lib/actor/address';
+import { HTTPAgent } from '@quenk/jhr/lib/agent';
+import { Request as HTTPRequest } from '@quenk/jhr/lib/request';
+import {
+    Response as HTTPResponse,
+    GenericResponse
+} from '@quenk/jhr/lib/response';
+
+import { App } from '../../app';
+import { Immutable } from '../../actor';
+
+export { HTTPRequest, HTTPResponse }
+
+/**
+ * Request are the type of messages that can be sent to a Remote to
+ * trigger a network request.
+ *
+ * Currently 3 modes are supported:
+ * 1. Send    - Single request mode.
+ * 2. SeqSend - Issues multiple requests sequentially.
+ * 3. ParSend - Issues multiple requests in parallel.
+ */
+export type Request<B>
+    = Send<B>
+    | ParSend<B>
+    | SeqSend<B>
+    ;
+
+/**
+ * Response the Remote received from the remote server.
+ * 
+ * This is either a single HTTPResponse or a wrapper for batch results.
+ */
+export type Response<T> = GenericResponse<T> | BatchResponse<T>;
+
+/**
+ * Send a single request to the remote host, forwarding the response to the
+ * specified address.
+ */
+export class Send<B> {
+
+    constructor(public client: Address, public request: HTTPRequest<B>) { }
+
+}
+
+/**
+ * ParSend sends a batch of requests to the remote host in sequentially,
+ * forwarding the combined responses to the specified address.
+ */
+export class SeqSend<B> {
+
+    constructor(public client: Address, public requests: HTTPRequest<B>[]) { }
+
+}
+
+/**
+ * ParSend sends a batch of requests to the remote host in parallel, forwarding
+ * the combined responses to the specified address.
+ */
+export class ParSend<B> {
+
+    constructor(public client: Address, public requests: HTTPRequest<B>[]) { }
+
+}
+
+/**
+ * TransportErr is a wrapper around errors that occur before the request
+ * reaches the remote end.
+ *
+ * Indicates we were unable to initiate the request for some reason, for example,
+ * the network is down or a Same-Origin policy violation.
+ */
+export class TransportErr {
+
+    constructor(public client: Address, public error: Err) { }
+
+    get message() {
+
+        return this.error.message;
+
+    }
+
+}
+
+/**
+ * BatchResponse is a combined list of responses for batch requests.
+ */
+export class BatchResponse<B> {
+
+    constructor(public value: HTTPResponse<B>[]) { }
+
+}
+
+/**
+ * Remote represents an HTTP server the app has access to.
+ *
+ * This actor is an abstraction over the `@quenk/jhr` so that requests
+ * can be sent via message passing. However, this abstraction is more 
+ * concerned with application level logic than the details of the HTTP
+ * protocols.
+ */
+export class Remote<Req, Res> extends Immutable<Request<Req>> {
+
+    constructor(
+        public agent: HTTPAgent<Req, Res>,
+        public system: App) { super(system); }
+
+    onUnit = ({ client, request }: Send<Req>) => {
+
+        let onErr = (e: Err) =>
+            this.tell(client, new TransportErr(client, e));
+
+        let onSucc = (res: HTTPResponse<Res>) =>
+            this.tell(client, res);
+
+        this
+            .agent
+            .send(request)
+            .fork(onErr, onSucc);
+
+    }
+
+    onParallel = ({ client, requests }: ParSend<Req>) => {
+
+        let { agent } = this;
+
+        let onErr = (e: Err) => this.tell(client, e);
+
+        let onSucc = (res: HTTPResponse<Res>[]) =>
+            this.tell(client, new BatchResponse(res));
+
+        let rs = requests.map((r: HTTPRequest<Req>) =>
+            agent.send(r).catch(e => raise(new TransportErr(client, e))));
+
+        parallel(rs).fork(onErr, onSucc);
+
+    }
+
+    onSequential = ({ client, requests }: SeqSend<Req>) => {
+
+        let { agent } = this;
+
+        let onErr = (e: Err) => this.tell(client, e);
+
+        let onSucc = (res: HTTPResponse<Res>[]) =>
+            this.tell(client, new BatchResponse(res));
+
+        let rs = requests.map((r: HTTPRequest<Req>) =>
+            agent.send(r).catch(e => raise(new TransportErr(client, e))));
+
+        sequential(rs).fork(onErr, onSucc);
+
+    }
+
+    receive: Case<Request<Req>>[] = <Case<Request<Req>>[]>[
+
+        new Case(Send, this.onUnit),
+
+        new Case(ParSend, this.onParallel),
+
+        new Case(SeqSend, this.onSequential)
+
+
+    ];
+
+    run() { }
+
+}
---
+++ src/app/remote/model.ts
@@ -0,0 +1,280 @@
+import { Future, fromCallback } from '@quenk/noni/lib/control/monad/future';
+import { Maybe, fromNullable } from '@quenk/noni/lib/data/maybe';
+import { Object } from '@quenk/noni/lib/data/jsonx';
+import { interpolate } from '@quenk/noni/lib/data/string';
+import { Address } from '@quenk/potoo/lib/actor/address';
+import { Instance } from '@quenk/potoo/lib/actor';
+import { Response } from '@quenk/jhr/lib/response';
+import { Post, Get, Patch, Delete } from '@quenk/jhr/lib/request';
+
+import { Id, Model } from '../model';
+import { SendCallback, CompleteHandler, ErrorBody } from './callback';
+import { JApp } from '../';
+import { TransportErr } from '.';
+
+/**
+ * SpawnFunc used by RemoteModels to spawn remote callbacks.
+ */
+export type SpawnFunc = (f: (s: JApp) => Instance) => Address;
+
+/**
+ * Result is the structure of the response body expected after a succesful
+ * CSUGR operation.
+ */
+export type Result<T extends Object>
+    = CreateResult
+    | SearchResult<T>
+    | GetResult<T>
+    | void
+    ;
+
+/**
+ * CreateResult is the response body expected after a successful create()
+ * operation.
+ */
+export interface CreateResult {
+
+    data: {
+
+        id: Id
+
+    }
+
+}
+
+/**
+ * SearchResult is the response body expected after a successful search()
+ * operation.
+ */
+export interface SearchResult<T extends Object> {
+
+    data: T[],
+
+    meta: {
+
+        pagination: Pagination
+
+    }
+
+}
+
+/**
+ * Pagination details of a successful search.
+ */
+export interface Pagination {
+
+    current: {
+
+        /**
+         * count of the current set.
+         */
+        count: number,
+
+        /**
+         * page number of the current set in the total result.
+         */
+        page: number,
+
+        /**
+         * limit indicates how many rows are allowed per page.
+         */
+        limit: number
+
+    },
+
+    total: {
+
+        /**
+         * count of the entire result set.
+         */
+        count: number,
+
+        /**
+         * pages available for the entire result.
+         */
+        pages: number
+
+    }
+
+}
+
+/**
+ * GetResult is the response body expected after a successful get()
+ * operation.
+ */
+export interface GetResult<T extends Object> {
+
+    data: T
+
+}
+
+/**
+ * FutureHandler is used to proxy the events of a request's response to a 
+ * Future.
+ *
+ * The handler provided also receives the events as they happen.
+ */
+export class FutureHandler<T extends Object>
+    implements
+    CompleteHandler<Result<T>>  {
+
+    constructor(
+        public handler: CompleteHandler<Result<T>>,
+        public onFailure: (err?: Error) => void,
+        public onSuccess: (r: Response<Result<T>>) => void) { }
+
+    onError(e: TransportErr) {
+
+        this.handler.onError(e);
+
+        this.onFailure(e.error instanceof Error ?
+            e.error :
+            new Error(e.error.message));
+
+    }
+
+    onClientError(r: Response<ErrorBody>) {
+
+        this.handler.onClientError(r);
+        this.onFailure(new Error(`ClientError: ${r.code}`));
+
+    }
+
+    onServerError(r: Response<ErrorBody>) {
+
+        this.handler.onServerError(r);
+        this.onFailure(new Error(`ServerError: ${r.code}`));
+
+    }
+
+    onComplete(r: Response<Result<T>>) {
+
+        this.handler.onComplete(r);
+        this.onSuccess(r);
+
+    }
+
+}
+
+/**
+ * RemoteModel provides a Model implementation that relies on Remote actors
+ * underneath.
+ *
+ * A handler can be provided to observe the result of requests if more data
+ * is needed than the Model api provides.
+ */
+export class RemoteModel<T extends Object> implements Model<T> {
+
+    constructor(
+        public remote: Address,
+        public path: string,
+        public spawn: SpawnFunc,
+        public handler: CompleteHandler<Result<T>>) { }
+
+    /**
+     * create a new entry for the data type.
+     */
+    create(data: T): Future<Id> {
+
+        return fromCallback(cb => {
+
+            this.spawn(s => new SendCallback(
+                s,
+                this.remote,
+                new Post(this.path, data),
+                new FutureHandler<T>(this.handler, cb, r => {
+
+                    cb(null, (<CreateResult>r.body).data.id);
+
+                })));
+
+        });
+
+    }
+
+    /**
+     * search for entries that match the provided query.
+     */
+    search(qry: Object): Future<T[]> {
+
+        return fromCallback(cb => {
+
+            this.spawn(s => new SendCallback(
+                s,
+                this.remote,
+                new Get(this.path, qry),
+                new FutureHandler(this.handler, cb, r => {
+
+                    cb(null, (r.code === 204) ?
+                        [] : (<SearchResult<T>>r.body).data);
+
+                })));
+
+        });
+
+    }
+
+    /**
+     * update a single entry using its id.
+     */
+    update(id: Id, changes: Partial<T>): Future<boolean> {
+
+        return fromCallback(cb => {
+
+            this.spawn(s => new SendCallback(
+                s,
+                this.remote,
+                new Patch(interpolate(this.path, { id }), changes),
+                new FutureHandler(this.handler, cb, r => {
+
+                    cb(null, (r.code === 200) ? true : false);
+
+                })));
+
+        });
+
+    }
+
+    /** 
+     * get a single entry by its id.
+     */
+    get(id: Id): Future<Maybe<T>> {
+
+        return fromCallback(cb => {
+
+            this.spawn(s => new SendCallback(
+                s,
+                this.remote,
+                new Get(interpolate(this.path, { id }), {}),
+                new FutureHandler(this.handler, cb, r => {
+
+                    cb(null, fromNullable((<GetResult<T>>r.body).data));
+
+                })));
+
+        });
+
+    }
+
+    /**
+     * remove a single entry by its id.
+     */
+    remove(id: Id): Future<boolean> {
+
+        return fromCallback(cb => {
+
+            this.spawn(s => new SendCallback(
+                s,
+                this.remote,
+                new Delete(interpolate(this.path, { id }), {}),
+                new FutureHandler(this.handler, cb, r => {
+
+                    cb(null, (r.code === 200) ? true : false);
+
+                })));
+
+        });
+
+    }
+
+}

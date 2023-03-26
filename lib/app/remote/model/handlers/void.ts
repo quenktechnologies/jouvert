06e9a40d230246ba9ec2d4d31dabc3c744b8f15d
Lasana Murray <lmurray@quenk.com>
2023-03-26 09:17:54 -0400

    build: stop checking in lib

--- lib/app/remote/model/handlers/void.ts
+++
@@ -1,9 +0,0 @@
-import { AbstractCompleteHandler } from '../../callback';
-
-/**
- * VoidHandler is a [[CompleteHandler]] that expects the body of the
- * result to be empty.
- *
- * It does nothing.
- */
-export class VoidHandler<T = void> extends AbstractCompleteHandler<T>{ }

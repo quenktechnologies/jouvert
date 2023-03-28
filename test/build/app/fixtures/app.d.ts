794514e03a0e0fcf5ee76ae9546803eda9f3e841
Lasana Murray <lmurray@quenk.com>
2023-03-28 16:54:15 -0400

    build: stop checking in test build

--- test/build/app/fixtures/app.d.ts
+++
@@ -1,5 +0,0 @@
-import { Address } from '@quenk/potoo/lib/actor/address';
-import { Jouvert, Template } from '../../../../lib/app';
-export declare class TestApp extends Jouvert {
-    spawn(temp: Template): Address;
-}
--- test/build/run.js
+++
@@ -1,6 +0,0 @@
-require("./app/router/hash_test.js");
-require("./app/remote/index_test.js");
-require("./app/remote/model_test.js");
-require("./app/remote/observer_test.js");
-require("./app/service/director_test.js");
-require("./app/search/filters_test.js");
